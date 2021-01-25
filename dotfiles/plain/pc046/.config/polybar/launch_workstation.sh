0a71d7801afdbb3543c309c2bcde5d6ca1905770
Marin Jurjevic <jurjevic@byte-lab.com>
2021-01-25 20:00:26 +0100

    Modified dotfiles/encrypt/common/.cert/nm-openvpn/bytelab-ca.pem, modified dotfiles/encrypt/common/.cert/nm-openvpn/bytelab-cert.pem, modified dotfiles/encrypt/common/.cert/nm-openvpn/bytelab-key.pem, modified dotfiles/encrypt/common/.cert/nm-openvpn/bytelab-tls-auth.pem, modified dotfiles/encrypt/common/.config/bytelab/nas/nasbl.conf, modified dotfiles/encrypt/common/.config/bytelab/openvpn/bytelab.conf, added dotfiles/encrypt/pc046/.ssh/config, added dotfiles/encrypt/pc046/.ssh/id_rsa, added dotfiles/encrypt/pc046/.ssh/id_rsa.pub, added dotfiles/plain/pc046/.config/autorandr/home/config, added dotfiles/plain/pc046/.config/autorandr/home/postswitch, added dotfiles/plain/pc046/.config/autorandr/home/setup, added dotfiles/plain/pc046/.config/autorandr/mobile/config, added dotfiles/plain/pc046/.config/autorandr/mobile/postswitch, added dotfiles/plain/pc046/.config/autorandr/mobile/setup, added dotfiles/plain/pc046/.config/autorandr/postswitch, added dotfiles/plain/pc046/.config/autorandr/workstation/config, added dotfiles/plain/pc046/.config/autorandr/workstation/postswitch, added dotfiles/plain/pc046/.config/autorandr/workstation/setup, added dotfiles/plain/pc046/.config/polybar/config, added dotfiles/plain/pc046/.config/polybar/config_home, added dotfiles/plain/pc046/.config/polybar/config_workstation, added dotfiles/plain/pc046/.config/polybar/launch.sh, added dotfiles/plain/pc046/.config/polybar/launch_home.sh, added dotfiles/plain/pc046/.config/polybar/launch_workstation.sh, added dotfiles/plain/pc046/.config/polybar/modules/system/cpu, added dotfiles/plain/pc046/.config/polybar/modules/system/ethernet, added dotfiles/plain/pc046/.config/polybar/modules/system/filesystem, added dotfiles/plain/pc046/.config/polybar/modules/system/memory, added dotfiles/plain/pc046/.config/polybar/modules/system/temperature, added dotfiles/plain/pc046/.config/polybar/modules/system/vpn, added dotfiles/plain/pc046/.config/polybar/modules/system/wifi, added dotfiles/plain/pc046/.i3/config, modified filelist

---
+++ dotfiles/plain/pc046/.config/polybar/launch_workstation.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE=~/.config/polybar/config_workstation
+
+# Terminate already running bar instances
+polybar-msg cmd quit
+
+# wait until all processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+echo "---" | tee -a /tmp/polybar_bottom.log /tmp/polybar_top.log
+echo "---" | tee -a /tmp/polybar_bottom2.log /tmp/polybar_top2.log
+echo "---" | tee -a /tmp/polybar_bottom3.log
+
+
+MONITOR="eDP1" polybar -l info bottom -c $CONFIG_FILE >>/tmp/polybar_bottom.log 2>&1 &
+MONITOR="eDP1" polybar -l info top -c $CONFIG_FILE >>/tmp/polybar_top.log 2>&1 &
+
+MONITOR="DP2-1" polybar -l info bottom2 -c $CONFIG_FILE >>/tmp/polybar_bottom2.log 2>&1 &
+MONITOR="DP2-1" polybar -l info top2 -c $CONFIG_FILE >/tmp/polybar_top2.log 2>&1 &
+
+MONITOR="DP2-2" polybar -l info bottom3 -c $CONFIG_FILE >/tmp/polybar_bottom3.log 2>&1 &
+
+echo "Bars launched..."
