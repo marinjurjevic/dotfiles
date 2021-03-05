4e9bb478e10de31af77ed9aa755abc3b38d17142
Marin Jurjevic <marin.jurjevic@hotmail.com>
2021-03-05 20:06:16 +0100

    polybar: fixed colors and layout on cerebrum

--- dotfiles/plain/cerebrum/.config/polybar/launch.sh
+++ dotfiles/plain/cerebrum/.config/polybar/launch.sh
@@ -13,7 +13,7 @@ echo "---" | tee -a /tmp/polybar_bottom2.log
 
 
 MONITOR="DP-4" polybar -l info bottom -c $CONFIG_FILE >>/tmp/polybar_bottom.log 2>&1 &
-MONITOR="DP-4" polybar -l info top -c $CONFIG_FILE >>/tmp/polybar_top.log 2>&1 &
+# MONITOR="DP-4" polybar -l info top -c $CONFIG_FILE >>/tmp/polybar_top.log 2>&1 &
 
 MONITOR="DP-0" polybar -l info bottom2 -c $CONFIG_FILE >>/tmp/polybar_bottom2.log 2>&1 &
 
