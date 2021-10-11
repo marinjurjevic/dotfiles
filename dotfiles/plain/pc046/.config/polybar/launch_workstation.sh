e5da87c8d003be6147d8f2b40aea138e967a6d0a
Marin Jurjević <marin.jurjevic@hotmail.com>
2021-10-11 16:22:55 +0200

    polybar: update workstation display identifiers

--- dotfiles/plain/pc046/.config/polybar/launch_workstation.sh
+++ dotfiles/plain/pc046/.config/polybar/launch_workstation.sh
@@ -13,12 +13,12 @@ echo "---" | tee -a /tmp/polybar_bottom2.log /tmp/polybar_top2.log
 echo "---" | tee -a /tmp/polybar_bottom3.log
 
 
-MONITOR="DP1" polybar -l info bottom -c $CONFIG_FILE >>/tmp/polybar_bottom.log 2>&1 &
+MONITOR="DP-1" polybar -l info bottom -c $CONFIG_FILE >>/tmp/polybar_bottom.log 2>&1 &
 # MONITOR="eDP1" polybar -l info top -c $CONFIG_FILE >>/tmp/polybar_top.log 2>&1 &
 
-MONITOR="DP2-1" polybar -l info bottom2 -c $CONFIG_FILE >>/tmp/polybar_bottom2.log 2>&1 &
+MONITOR="DP-2-1" polybar -l info bottom2 -c $CONFIG_FILE >>/tmp/polybar_bottom2.log 2>&1 &
 # MONITOR="DP2-1" polybar -l info top2 -c $CONFIG_FILE >/tmp/polybar_top2.log 2>&1 &
 
-MONITOR="DP2-2" polybar -l info bottom3 -c $CONFIG_FILE >/tmp/polybar_bottom3.log 2>&1 &
+MONITOR="DP-2-2" polybar -l info bottom3 -c $CONFIG_FILE >/tmp/polybar_bottom3.log 2>&1 &
 
 echo "Bars launched..."
